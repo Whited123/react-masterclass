832f6b3318388743038b76ec29c49d030c8e077c
kim dong young <dudtodtls24@gmail.com>
2023-06-02 18:11:30 +0900

    리액트 mc 2.6
    수퍼 복습.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: tomato;
+  &:hover {
+    color: teal;
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
   justify-content: center;
   align-items: center;
-`;
-
-const animation = keyframes`
- 0%{
-transform: rotate(0deg);
-border-radius: 0px;
- }
- 50%{
-  transform: rotate(360deg);
-  border-radius: 100px;
-}
-100%{
-  transform: rotate(0deg);
-border-radius: 0px;
-}
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${animation} 1s linear infinite;
-  ${Emoji}:hover {
+  ${Title}:hover {
     font-size: 98px;
   }
 `;
@@ -43,10 +21,7 @@ const Box = styled.div`
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">웃는이모지</Emoji>
-      </Box>
-      <Emoji as="p">웃는이모지</Emoji>
+      <Title>안녕</Title>
     </Wrapper>
   );
 }
