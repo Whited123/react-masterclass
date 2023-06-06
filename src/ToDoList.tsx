8fb4611d677038442de37067800a9b45b10ca2eb
kim dong young <dudtodtls24@gmail.com>
2023-06-06 20:26:42 +0900

    리액트 mc 6.6
    리액트 훅폼을 이용하면 정말 코드가 단축된다. register: 양식을 설정해두고 watch: 해당 값을 주시하도록한다.

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,46 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+//   const [toDoError, setToDoError] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDoError("");
+//     setToDo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (toDo.length < 10) {
+//       return setToDoError("글자 수가 10보다 커야합니다.");
+//     }
+//     console.log("제 출 당 당");
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={toDo} placeholder="할 일 적기" />
+//         <button>추가하기</button>
+//         {toDoError !== "" ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="할 일 적기" />
+      <form>
+        <input {...register("이메일")} placeholder="이메일" />
+        <input {...register("성")} placeholder="성" />
+        <input {...register("이름")} placeholder="이름" />
+        <input {...register("아이디")} placeholder="아이디" />
+        <input {...register("비밀번호")} placeholder="비밀번호" />
+        <input {...register("비밀번호 확인")} placeholder="비밀번호 확인" />
         <button>추가하기</button>
       </form>
     </div>
